ad89bb87065d435fc402bd06dcd0eeefd36f868a
Maverick Tesini <mavericktesini@gmail.com>
2017-11-08 21:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function distanceFromHqInFeet(distance) {
  }
 
 function calculatesFarePrice(start, destination) {
-  feet2 = distanceTravelledInFeet(start, destination) * 1;
-  switch (cost) {
+  distance = distanceTravelledInFeet(start, destination) * 1;
+  switch (distance) {
     case (feet2 >= 400):
         return 0;
     case (feet2 > 400 && feet2 <2000):
