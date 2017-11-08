b54d9424f0fbb80b6e8ce33d0131154a104c0427
Maverick Tesini <mavericktesini@gmail.com>
2017-11-08 20:40:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blocks, distance) {
   distance = 8;
-  blocks = 42 - distance;
+  blocks = 16 - distance;
   return blocks;
 }
 
