f7619e4094322612d64cde5721aa1cdb3258ac48
Maverick Tesini <mavericktesini@gmail.com>
2017-11-08 21:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function calculatesFarePrice(start, destination) {
     } else if (feet > 400 && feet < 2000) {
       return feet * 0.02;
     } else if (feet > 2000 && feet < 2500) {
-      return 100;
+      return 25;
     } else if (feet => 2500) {
       return 'cannot travel that far';
     }
